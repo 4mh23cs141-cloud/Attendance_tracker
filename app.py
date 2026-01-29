5f211131038830874ec9a103f8d7d3e1624bda59
4mh23cs141-cloud <4mh23cs141@gmail.com>
2026-01-29 09:31:55 +0530

    changed version

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ def delete_student(student_name):
     return jsonify({'success': False, 'message': 'Student not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
